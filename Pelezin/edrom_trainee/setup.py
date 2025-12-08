403e033aa08938a54a85766c373b4581388ba898
Guilherme Enrique de Oliveira Nascimento <guilhermenascimentooli10@gmail.com>
2025-12-08 20:21:42 -0300

    Add files via upload

---
+++ Pelezin/edrom_trainee/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'edrom_trainee'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='guilherme-enrique',
+    maintainer_email='guilherme-enrique@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        	'controlador = edrom_trainee.meu_robo:main',
+        ],
+    },
+)
